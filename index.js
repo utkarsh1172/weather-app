d0f78896c4fdc0a11947f56ab90070e36d7ccb91
Utkarsh Adhikrao Kamble <utkarsh1172@gmail.com>
2022-09-28 17:48:27 +0530

    second commit

--- index.js
+++ index.js
@@ -1,39 +1,92 @@
-const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`;
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 
-const form = document.querySelector("form");
-const weather = document.querySelector("#weather");
-const search = document.querySelector("#search");
+const weatherApi = {
+  key: "bab281d79e5f1e9755a68d754cc313e7",
+  baseUrl: "https://api.openweathermap.org/data/2.5/weather", 
+}
 
+const searchInputBox = document.getElementById('input-box');
 
-const getWeather = async (city) => {
-    weather.innerHTML = `<h2>Loading...</h4>`
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return showWeather(data);
-};
+// Event Listener Function on keypress
+searchInputBox.addEventListener('keypress', (event) => {
+  
+  if(event.keyCode == 13) {
+      console.log(searchInputBox.value);
+      getWeatherReport(searchInputBox.value);
+      document.querySelector('.weather-body').style.display = "block";
+  }
 
-const showWeather = (data) => {
-   if(data.cod == '404'){
-    weather.innerHTML = `<h2>City Not Found</h4>`
-   }
-  weather.innerHTML = 
-  `
-  <div>
-    <img src= "https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-  </div>
-  <div>
-    <h2> ${data.main.temp}</h2>
-    <h4 style="margin-top:-10px;">${data.weather[0].main}</h4>
-  </div>
-`
-};
-
-form.addEventListener("submit", function (event) {
-  getWeather(search.value);
-  event.preventDefault();
 });
 
-// const API =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+// Get Weather Report
+function getWeatherReport(city) {
+  
+  fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+  .then(weather => {
+      return weather.json();
+  }).then(showWeatherReport);
+}
 
-// const IMG_URL = "https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png";
+// Show Weather Report
+function showWeatherReport(weather){
+  console.log(weather);
+
+  let city = document.getElementById('city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let temperature = document.getElementById('temp');
+  temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+  let minMaxTemp = document.getElementById('min-max');
+  minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
+
+  let weatherType = document.getElementById('weather');
+  weatherType.innerText = `${weather.weather[0].main}`;
+
+  let date = document.getElementById('date');
+  let todayDate = new Date();
+  date.innerText = dateManage(todayDate);
+
+  
+  if(weatherType.textContent == 'Clear') {
+      document.body.style.backgroundImage = "url('images/clear.jpg')";
+      
+  } else if(weatherType.textContent == 'Clouds') {
+
+      document.body.style.backgroundImage = "url('images/cloud.jpg')";
+      
+  } else if(weatherType.textContent == 'Haze') {
+
+      document.body.style.backgroundImage = "url('images/cloud.jpg')";
+      
+  }     else if(weatherType.textContent == 'Rain') {
+      
+      document.body.style.backgroundImage = "url('images/rain.jpg')";
+      
+  } else if(weatherType.textContent == 'Snow') {
+      
+      document.body.style.backgroundImage = "url('images/snow.jpg')";
+  
+  } else if(weatherType.textContent == 'Thunderstorm') {
+  
+      document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
+      
+  } 
+}
+
+// Date manage
+function dateManage(dateArg) {
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  let year = dateArg.getFullYear();
+  let month = months[dateArg.getMonth()];
+  let date = dateArg.getDate();
+  let day = days[dateArg.getDay()];
+
+  return `${date} ${month} (${day}), ${year}`;
+}
+
+
